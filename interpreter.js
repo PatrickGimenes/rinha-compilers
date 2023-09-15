72afd612ce2468dbe90a585bfcff20debc7a924f
Patrick Gimenes <48632980+PatrickGimenes@users.noreply.github.com>
2023-09-15 01:01:06 -0300

    feat: interpreting binary op

--- interpreter.js
+++ interpreter.js
@@ -17,6 +17,39 @@ function interpreter(node) {
       console.log(term);
       break;
 
+    case "Binary":
+      const lhs = interpreter(node.lhs);
+      const rhs = interpreter(node.rhs);
+
+      switch (node.op) {
+        case "Add":
+          return lhs + rhs;
+        case "Sub":
+          return lhs - rhs;
+        case "Mul":
+          return lhs * rhs;
+        case "Div":
+          return rhs === 0 ? lhs / rhs : console.error("Divisão por 0!");
+        case "Rem":
+          return lhs % rhs;
+        case "Eq":
+          return lhs === rhs;
+        case "Neq":
+          return lhs !== rhs;
+        case "Lt":
+          return lhs < rhs;
+        case "Gt":
+          return lhs > rhs;
+        case "Lte":
+          return lhs <= rhs;
+        case "Gte":
+          return lhs >= rhs;
+        case "And":
+          return lhs && rhs;
+        case "Or":
+          return lhs || rhs;
+      }
+
     default:
       console.log("Erro!");
   }
